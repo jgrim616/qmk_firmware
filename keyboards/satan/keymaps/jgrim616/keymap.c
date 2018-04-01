6a1971bfc3e74103f04981b2a0cd4dead845fa53
Jim Grimaldi <jgrimaldi@healthesystems.com>
2018-03-31 20:18:17 -0400

    - Copying complete 1upkeyboards keymap.c file over as a staring point for jgrim616

--- keyboards/satan/keymaps/jgrim616/keymap.c
+++ keyboards/satan/keymaps/jgrim616/keymap.c
@@ -9,7 +9,7 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BL 0 // Base Layer
-#define _FL 1 // Function Layer
+#define _FL 1 // Funtion Layer
 
 #define _______ KC_TRNS
 
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
    * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |FN  |Ctrl |
+   * |Ctrl|Gui |Alt |      Space            |Alt |FN  |Gui |Ctrl |
    * `-----------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
@@ -32,17 +32,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
   KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSFT, \
-  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,KC_RGUI, MO(_FL),KC_RCTL),
+  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,MO(_FL), KC_RGUI,KC_RCTL),
 
   /* Keymap _FL: Function Layer
    * ,-----------------------------------------------------------.
-   * |   |   |   |   |   |   |   |   |   |   |   |   |   |  RESET|
+   * |   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12| DELETE|
    * |-----------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |   |   |   |BL-|BL+|BL   |
+   * |     |   |Up |   |   |   |Cal|   |Ins|   |Prt|Scr|Pau|     |
    * |-----------------------------------------------------------|
-   * |      |   |   |   |   |   |   |       |   |   |   |        |
+   * |      |Lt |Dn |Rt |   |   |   |   |   |   |Hme|PgU|        |
    * |-----------------------------------------------------------|
-   * |        | F1|F2 | F3|F4 | F5| F6| F7| F8|   |   |          |
+   * |        |   |Cxt|BL-|BLT|BL+|Vo-|Vo+|Mut|End|PgD|          |
    * |-----------------------------------------------------------|
    * |    |    |    |                        |    |    |    |    |
    * `-----------------------------------------------------------'
@@ -55,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,RGB_TOG,RGB_MOD,RGB_HUI,RGB_HUD,RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD,_______,_______,_______, \
   _______,_______,_______,                 _______,                       _______,_______,_______, _______),
   #else
-  KC_GRV, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET,  \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, BL_DEC, BL_INC,BL_TOGG, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,                _______,                        _______,_______,_______,_______),
+  KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL,  \
+  _______,_______,KC_UP,  _______,_______,_______,KC_CALC,_______,KC_INS, _______,KC_PSCR,KC_SLCK,KC_PAUS,_______, \
+  _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,_______,_______,_______,_______,KC_HOME,KC_PGUP,_______, \
+  _______,_______,KC_MENU,BL_DEC, BL_TOGG,BL_INC, KC_VOLD,KC_VOLU,KC_MUTE,KC_END, KC_PGDN,_______, \
+  _______,RESET,  _______,                _______,                        _______,_______,_______,_______),
   #endif
 };
 
