81d4620e9e263d4b517154331824af4400a306e1
Jim Grimaldi <jgrimaldi@healthesystems.com>
2018-04-02 20:07:59 -0400

    - Added Windows Base Layer and Function Layer

--- keyboards/satan/keymaps/jgrim616/keymap.c
+++ keyboards/satan/keymaps/jgrim616/keymap.c
@@ -9,12 +9,14 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _MBL 0 // Mac Base Layer
-#define _MFL 1 // Mac Funtion Layer
+#define _WBL 1 // Windows Base Layer
+#define _MFL 2 // Mac Funtion Layer
+#define _WFL 3 // Windows Function Layer
 
 #define _______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: (Base Layer) Default Layer
+  /* Keymap _MBL: (Mac Base Layer) Default Layer
    * ,-----------------------------------------------------------.
    * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
    * |-----------------------------------------------------------|
@@ -34,7 +36,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSFT, \
   KC_LCTL, KC_LALT,KC_LGUI,                KC_SPC,                                 KC_RGUI,MO(_MFL),KC_RALT,KC_RCTL),
 
-  /* Keymap _FL: Function Layer
+  /* Keymap _WBL: (Windows Base Layer)
+   * ,-----------------------------------------------------------.
+   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
+   * |-----------------------------------------------------------|
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
+   * |-----------------------------------------------------------|
+   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |-----------------------------------------------------------|
+   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
+   * |-----------------------------------------------------------|
+   * |Ctrl|Gui |Alt |      Space            |Alt |FN  |Gui |Ctrl |
+   * `-----------------------------------------------------------'
+   */
+[_WBL] = KEYMAP_ANSI(
+  F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
+  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
+  KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
+  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSFT, \
+  KC_LCTL, KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,MO(_WFL),KC_RGUI,KC_RCTL),
+
+  /* Keymap _MFL: Mac Function Layer
    * ,-----------------------------------------------------------.
    * |   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12| DELETE|
    * |-----------------------------------------------------------|
@@ -59,7 +81,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,KC_UP,  _______,_______,_______,KC_CALC,_______,KC_INS, _______,KC_PSCR,KC_SLCK,KC_PAUS,KC_PWR, \
   _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,_______,_______,_______,_______,KC_HOME,KC_PGUP,_______, \
   _______,_______,KC_MENU,BL_DEC, BL_TOGG,BL_INC, KC_VOLD,KC_VOLU,KC_MUTE,KC_END, KC_PGDN,_______, \
-  _______,RESET,  _______,                _______,                        _______,_______,_______,_______),
+  _______,RESET,  _______,                _______,                        _______,_______,TO(_WBL),TO(_MBL) ),
+  #endif
+
+  /* Keymap _WFL: Windows Function Layer
+   * ,-----------------------------------------------------------.
+   * |   |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12| DELETE|
+   * |-----------------------------------------------------------|
+   * |     |   |Up |   |   |   |Cal|   |Ins|   |Prt|Scr|Pau|SLEEP|
+   * |-----------------------------------------------------------|
+   * |      |Lt |Dn |Rt |   |   |   |   |   |   |Hme|PgU|        |
+   * |-----------------------------------------------------------|
+   * |        |   |Cxt|BL-|BLT|BL+|Vo-|Vo+|Mut|End|PgD|          |
+   * |-----------------------------------------------------------|
+   * |    |    |    |                        |    |    |    |    |
+   * `-----------------------------------------------------------'
+   */
+[_WFL] = KEYMAP_ANSI(
+  #ifdef RGBLIGHT_ENABLE
+  KC_GRV, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RESET,  \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, BL_DEC,BL_INC, BL_TOGG, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
+  _______,RGB_TOG,RGB_MOD,RGB_HUI,RGB_HUD,RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD,_______,_______,_______, \
+  _______,_______,_______,                 _______,                       _______,_______,_______, _______),
+  #else
+  KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL,  \
+  _______,_______,KC_UP,  _______,_______,_______,KC_CALC,_______,KC_INS, _______,KC_PSCR,KC_SLCK,KC_PAUS,KC_SLEP, \
+  _______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,_______,_______,_______,_______,KC_HOME,KC_PGUP,_______, \
+  _______,_______,KC_MENU,BL_DEC, BL_TOGG,BL_INC, KC_VOLD,KC_VOLU,KC_MUTE,KC_END, KC_PGDN,_______, \
+  _______,RESET,  _______,                _______,                        _______,_______,TO(_WBL),TO(_MBL) ),
   #endif
 };
 
