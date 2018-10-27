56a94d91b7169dc3fda9df63a387c886308ba4d9
Jim Grimaldi <jgrimaldi@healthesystems.com>
2018-10-26 21:43:46 -0400

    Quick Fix

--- keyboards/satan/keymaps/jgrim616_spacefn/keymap.c
+++ keyboards/satan/keymaps/jgrim616_spacefn/keymap.c
@@ -18,16 +18,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		LSFT(KC_NO), KC_Z,          KC_X, KC_C,  KC_V,          KC_B,        KC_N,        KC_M,  KC_COMM, KC_DOT, KC_SLSH, RSFT(KC_NO), 
 		LCTL(KC_NO), LGUI_T(KC_NO), LALT(KC_NO), LT(1, KC_SPC), RALT(KC_NO), RGUI(KC_NO), MO(2), RCTL(KC_NO)),
 	[1] = LAYOUT_60_ansi(
-		KC_GRV, KC_F1, KC_F2, KC_F3,  KC_F4, KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11, KC_F12, KC_DEL, 
-		KC_NO,  KC_NO, KC_NO, KC_ESC, KC_NO, KC_NO,  KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_NO,  KC_NO,  KC_NO,  KC_NO, 
-		KC_NO,  KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,  KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,  KC_NO,  KC_NO, 
-		KC_NO,  KC_NO, KC_NO, KC_NO,  KC_NO, KC_SPC, KC_PGDN, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO, 
-		KC_NO,  KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,  KC_TRNS, KC_NO),
+		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, 
+		_______, _______, _______, KC_ESC,  _______, _______, _______, KC_HOME, KC_UP,   KC_END,  _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______, KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, 
+		_______, _______, _______, _______, _______, KC_SPC,  KC_PGDN, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______, KC_TRNS, _______),
 	[2] = LAYOUT_60_ansi(
-		KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-		KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-		KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, 
-		KC_NO, KC_NO, KC_NO, BL_DEC, BL_TOGG, BL_INC, KC_VOLD, KC_VOLU, KC_MUTE, KC_NO, KC_NO, KC_NO, 
-		KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,   KC_NO,  KC_TRNS, KC_NO)
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+		_______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, 
+		_______, _______, _______, _______, _______,  _______, KC_TRNS,   _______)
 };
 
