c5dd0dba4a2c719b7e25a8cf328c26bca72726ff
Jim Grimaldi <jgrimaldi616@gmail.com>
2018-12-22 02:11:14 -0500

    Keymap Updates
      - Adding GAMING Layer and reordering so BASE LAYERS are at the top

--- keyboards/1upkeyboards/1up60hse/keymaps/jgrim616_spacefn/keymap.c
+++ keyboards/1upkeyboards/1up60hse/keymaps/jgrim616_spacefn/keymap.c
@@ -19,10 +19,10 @@
 // Helpful defines
 #define _______ KC_TRNS
 #define _DEFAULT 0
-#define _MOVEMENT 1
-#define _FUNCTION 2
-#define _MACROS 3
-#define _GAMING 4
+#define _GAMING 1
+#define _MOVEMENT 2
+#define _FUNCTION 3
+#define _MACROS 4
 
 // Keyboard Layout
 // 14
@@ -40,13 +40,23 @@ enum custom_keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // Layer 0 - Cyan
   [_DEFAULT] = LAYOUT_60_ansi(
     KC_ESC,      KC_1,          KC_2,        KC_3,              KC_4,              KC_5,                  KC_6,           KC_7,                  KC_8,    KC_9,     KC_0,    KC_MINS,            KC_EQL,  KC_BSPC,
     KC_TAB,      KC_Q,          KC_W,        KC_E,              KC_R,              KC_T,                  KC_Y,           KC_U,                  KC_I,    KC_O,     KC_P,    KC_LBRC,            KC_RBRC, KC_BSLS,
-    LCTL(KC_NO), KC_A,          LT(1, KC_S), MT(MOD_LGUI,KC_D), MT(MOD_LALT,KC_F), MT(MOD_LCTL, KC_G),    KC_H,           KC_J,                  KC_K,    KC_L,     KC_SCLN, KC_QUOT,            KC_ENT,
+    LCTL(KC_NO), KC_A,          LT(2, KC_S), MT(MOD_LGUI,KC_D), MT(MOD_LALT,KC_F), MT(MOD_LCTL, KC_G),    KC_H,           KC_J,                  KC_K,    KC_L,     KC_SCLN, KC_QUOT,            KC_ENT,
     LSFT(KC_NO), KC_Z,          KC_X,        KC_C,              KC_V,              KC_B,                  KC_N,           KC_M,                  KC_COMM, KC_DOT,   KC_SLSH, MT(MOD_RSFT, KC_UP),
-    LCTL(KC_NO), LGUI_T(KC_NO), LALT(KC_NO), LT(1, KC_SPC),     RALT(KC_NO),       MT(MOD_RGUI, KC_LEFT), LT(2, KC_DOWN), MT(MOD_RCTL, KC_RGHT)),
+    LCTL(KC_NO), LGUI_T(KC_NO), LALT(KC_NO), LT(2, KC_SPC),     RALT(KC_NO),       MT(MOD_RGUI, KC_LEFT), LT(3, KC_DOWN), MT(MOD_RCTL, KC_RGHT)),
 
+  // Layer 1 - Purple
+  [_GAMING] = LAYOUT_60_ansi(
+    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_RGUI, MO(3), KC_LCTL),
+
+  // Layer 2 - Green
   [_MOVEMENT] = LAYOUT_60_ansi(
     KC_GRV,      KC_F1,        KC_F2,        KC_F3,            KC_F4,            KC_F5,                   KC_F6,          KC_F7,                 KC_F8,   KC_F9,    KC_F10,  KC_F11,             KC_F12,  KC_DEL,
     _______,     _______,      _______,      KC_ESC,           _______,          _______,                 _______,        KC_HOME,               KC_UP,   KC_END,   _______, _______,            _______, _______,
@@ -54,13 +64,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,     _______,      KC_DEL,       _______,          _______,          KC_SPC,                  _______,        _______,               KC_PGUP, KC_PGDN,  _______, _______,
     _______,     _______,      _______,      _______,          _______,          _______,                 _______,        _______),
 
+  // Layer 3 - White
   [_FUNCTION] = LAYOUT_60_ansi(
     _______,     _______,      _______,      _______,          _______,          _______,                 _______,        _______,               _______, _______,  _______, _______,            _______, RESET,
-    BL_TOGG,     BL_INC,       BL_DEC,       BL_STEP,          _______,          _______,                 _______,        _______,               _______, KC_MSTP,  KC_MPLY, KC_MRWD,            KC_MFFD, _______,
+    BL_TOGG,     BL_INC,       BL_DEC,       BL_STEP,          _______,          _______,                 _______,        _______,               _______, KC_MSTP,  KC_MPLY, KC_MRWD,            KC_MFFD, TG(1),
     RGB_TOG,     RGB_MOD,      RGB_HUI,      RGB_SAI,          RGB_VAI,          RGB_SPI,                 RGB_M_P,        RGB_M_B,               RGB_M_R, RGB_M_SW, _______, _______,            _______,
     _______,     RGB_RMOD,     RGB_HUD,      RGB_SAD,          RGB_VAD,          RGB_SPD,                 RGB_M_SN,       RGB_M_K,               RGB_M_X, RGB_M_G,  _______, _______,
-    KC_MUTE,     KC_VOLD,      KC_VOLU,      _______,          _______,          MO(3),                   _______,        _______),
+    KC_MUTE,     KC_VOLD,      KC_VOLU,      _______,          _______,          MO(4),                   _______,        _______),
 
+  // Layer 4 - Red
   [_MACROS] = LAYOUT_60_ansi(
     _______,     _______,      _______,      _______,          _______,          _______,                 _______,        _______,               _______, _______, _______, _______,             _______, _______,
     _______,     _______,      _______,      M_EMAIL,          _______,          _______,                 _______,        M_USER,                _______, _______, M_PASS1, M_PASS2,             _______, _______,
