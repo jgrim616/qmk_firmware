c814350a8e168f36a8ce8908a1471e130c40735c
Jim Grimaldi <jgrimaldi616@gmail.com>
2018-12-22 17:53:52 -0500

    Keymap Updates
      - Quick changes to the movement layer

--- keyboards/1upkeyboards/1up60hse/keymaps/jgrim616_spacefn/keymap.c
+++ keyboards/1upkeyboards/1up60hse/keymaps/jgrim616_spacefn/keymap.c
@@ -59,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Layer 2 - Green
   [_MOVEMENT] = LAYOUT_60_ansi(
     KC_GRV,      KC_F1,        KC_F2,        KC_F3,            KC_F4,            KC_F5,                   KC_F6,          KC_F7,                 KC_F8,   KC_F9,    KC_F10,  KC_F11,             KC_F12,  KC_DEL,
-    _______,     _______,      _______,      KC_ESC,           _______,          _______,                 _______,        KC_HOME,               KC_UP,   KC_END,   _______, _______,            _______, _______,
+    _______,     _______,      _______,      KC_ENT,           _______,          _______,                 _______,        KC_HOME,               KC_UP,   KC_END,   _______, _______,            _______, _______,
     _______,     _______,      _______,      _______,          _______,          _______,                 KC_NO,          KC_LEFT,               KC_DOWN, KC_RGHT,  _______, _______,            _______,
-    _______,     _______,      KC_DEL,       _______,          _______,          KC_SPC,                  _______,        _______,               KC_PGUP, KC_PGDN,  _______, _______,
+    _______,     _______,      KC_DEL,       KC_BSPC,          _______,          KC_SPC,                  _______,        _______,               KC_PGUP, KC_PGDN,  _______, _______,
     _______,     _______,      _______,      _______,          _______,          _______,                 _______,        _______),
 
   // Layer 3 - White
