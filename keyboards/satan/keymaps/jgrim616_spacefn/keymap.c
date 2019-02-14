ec1aaf6506c63240476644894231f6058255cee2
Jim Grimaldi <jgrimaldi@healthesystems.com>
2019-02-14 13:15:21 -0500

    Updating Satan keymap

--- keyboards/satan/keymaps/jgrim616_spacefn/keymap.c
+++ keyboards/satan/keymaps/jgrim616_spacefn/keymap.c
@@ -3,12 +3,12 @@
 // Helpful defines
 #define _______ KC_TRNS
 
-// Keyboard Layout
-// 14
-// 14
-// 13
-// 12
-// 8
+// Keyboard Layout (# of keys per row)
+// R1 - 14
+// R2 - 14
+// R3 - 13
+// R4 - 12
+// R5 - 8
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_60_ansi(
@@ -19,9 +19,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		LCTL(KC_NO), LGUI_T(KC_NO), LALT(KC_NO), LT(1, KC_SPC),     RALT(KC_NO),       MT(MOD_RGUI, KC_LEFT), LT(2, KC_DOWN), MT(MOD_RCTL, KC_RGHT)),
 	[1] = LAYOUT_60_ansi(
 		KC_GRV,      KC_F1,        KC_F2,        KC_F3,            KC_F4,            KC_F5,                   KC_F6,          KC_F7,                 KC_F8,   KC_F9,   KC_F10,  KC_F11,             KC_F12,  KC_DEL, 
-		_______,     _______,      _______,      KC_ESC,           _______,          _______,                 _______,        KC_HOME,               KC_UP,   KC_END,  _______, _______,            _______, _______, 
+		_______,     _______,      _______,      KC_ENT,           _______,          _______,                 _______,        KC_HOME,               KC_UP,   KC_END,  _______, _______,            _______, _______, 
 		_______,     _______,      _______,      _______,          _______,          _______,                 KC_NO,          KC_LEFT,               KC_DOWN, KC_RGHT, _______, _______,            _______, 
-		_______,     _______,      KC_DEL,       _______,          _______,          KC_SPC,                  _______,        _______,               KC_PGUP, KC_PGDN, _______, _______, 
+		_______,     _______,      KC_DEL,       KC_BSPC,          _______,          KC_SPC,                  _______,        _______,               KC_PGUP, KC_PGDN, _______, _______, 
 		_______,     _______,      _______,      _______,          _______,          _______,                 _______,        _______),
 	[2] = LAYOUT_60_ansi(
 		_______,     _______,      _______,      _______,          _______,          _______,                 _______,        _______,               _______, _______, _______, _______,            _______, _______, 
